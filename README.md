b39057413577f30e31623111e4a57479e028fae1
Sven Fritz <sven.fritz@blue-yonder.com>
2019-06-15 19:45:18 +0200

    Solutions to rustlings

---
+++ README.md
@@ -0,0 +1,3 @@
+# solutions to rustlings v1.3.0 (https://github.com/rust-lang/rustlings)
+
+This is my first practical contact with rust, so solutions will not always be optimal.
---
+++ exercises/tests/tests3.rs
@@ -0,0 +1,52 @@
+// tests3.rs
+// This test isn't testing our function -- make it do that in such a way that
+// the test passes. Then write a second test that tests whether we get the result
+// we expect to get when we call `is_even(5)`. Scroll down for hints!
+
+pub fn is_even(num: i32) -> bool {
+    num % 2 == 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_true_when_even() {
+        let num = 42;
+        assert!(is_even(num));
+    }
+
+    #[test]
+    fn result_as_expected() {
+        let num = 5;
+        let expected_result = false;
+        let actual_result = is_even(num);
+        assert_eq!(expected_result, actual_result);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// You can call a function right where you're passing arguments to `assert!` -- so you could do
+// something like `assert!(having_fun())`. If you want to check that you indeed get false, you
+// can negate the result of what you're doing using `!`, like `assert!(!having_fun())`.
